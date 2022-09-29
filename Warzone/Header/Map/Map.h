232d8a77ab44279a94b89d1452d965ed897d2e18
Nada EA <nada.elarabi2@gmail.com>
2022-09-29 12:49:06 -0400

    Just checking if git is updating github properly

--- Warzone/Header/Map/Map.h
+++ Warzone/Header/Map/Map.h
@@ -1,8 +1,13 @@
+#include <vector>
+#include <string>
+#include <iostream>
+using namespace std;
 
-/*class Map
+
+class Map
 {
 
-};*/
+};
 
 class MapLoader {
 
@@ -15,14 +20,29 @@ public:
 	
 };
 
-/*struct Continents {
+struct Continents {
+	int ContinentID;
+	std::string ContinentName;
+	Continents(int contID, std::string contName);
 
-};*/
+};
 
-/*class Territory {
+class Territory {
+	
+	private:
+	int teritorryID;
+	std::string TeritorryName;
+	vector<Territory*> arrOfAdjTerritories;
+	int* IDOfContTerritoryBelongsTo;//Potentially to know which continent the territory belongs to?
+
+	public:
+		Territory(int TerrID, std::string TerrName, int ContID);
+		Territory(const Territory& CopyOfTerritory);//Copy constructor for Territory class
+
+
 	
 
-	int teritorryID;
 
-};*/
+
+};
 
