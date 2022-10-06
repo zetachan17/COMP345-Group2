0a930a2cefa9cc214dc845810f1e8bfc2c705a5d
Rafa <57460462+rfpcmaia@users.noreply.github.com>
2022-10-05 22:52:17 -0400

    Moved files to their header and source folders

--- Warzone/Header/Orders/Orders.h
+++ Warzone/Header/Orders/Orders.h
@@ -1,5 +1,77 @@
 #pragma once
-class Orders
+#include <vector>
+#include <string>
+using std::string;
+using std::vector;
+
+class Order
 {
+public:
+    Order(string name);
+    bool validate();
+    void execute();
+    string getName() const;
+
+private:
+    string name;
 };
 
+class Deploy : public Order
+{
+public:
+    Deploy();
+    bool validate();
+    void execute();
+};
+
+class Advance : public Order
+{
+public:
+    Advance();
+    bool validate();
+    void execute();
+};
+
+class Bomb : public Order
+{
+public:
+    Bomb();
+    bool validate();
+    void execute();
+};
+
+class Blockade : public Order
+{
+public:
+    Blockade();
+    bool validate();
+    void execute();
+};
+
+class Airlift : public Order
+{
+public:
+    Airlift();
+    bool validate();
+    void execute();
+};
+
+class Negotiate : public Order
+{
+public:
+    Negotiate();
+    bool validate();
+    void execute();
+};
+
+class OrdersList
+{
+public:
+    void issue(Order *newOrder);
+    void move(int pos, int newPos);
+    void remove(int pos);
+    void output() const;
+
+private:
+    vector<Order *> m_Orders;
+};
