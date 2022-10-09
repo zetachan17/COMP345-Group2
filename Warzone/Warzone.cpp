98f84075e2a1d2a4edc1f7489a1790eb4d043282
Runze Zhu <z_runze@live.concordia.ca>
2022-10-09 19:41:31 -0400

    delete unnecessary includes in main

--- Warzone/Warzone.cpp
+++ Warzone/Warzone.cpp
@@ -1,13 +1,9 @@
 // Warzone.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#include <iostream>
-#include "Cards/Cards.h"
 #include "Cards/CardsDriver.h"
 
 int main()
 {
-    std::cout << "Hello World!\n";
-
+    //P4
     testCards();
-
 }
