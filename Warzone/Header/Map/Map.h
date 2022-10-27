0e293bdc512ce31ce8225b9b33fcae7f36ee06f5
Rafa <57460462+rfpcmaia@users.noreply.github.com>
2022-10-26 21:33:00 -0400

    fixed issue with destructors/segmentation fault

--- Warzone/Header/Map/Map.h
+++ Warzone/Header/Map/Map.h
@@ -3,122 +3,104 @@
 #include <iostream>
 using namespace std;
 
+class Territory
+{
 
-
-
-
-class Territory {
-	
-	private:
+private:
 	int territoryID;
-	string TeritorryName;
+	string TerritoryName;
 
-	
-	vector<Territory*> arrOfAdjTerritories;
-	
-	int ContinentId;//Potentially to know which continent the territory belongs to?
-	
+	vector<Territory *> arrOfAdjTerritories;
 
-	public:
-		Territory(int TerrID, std::string TerrName, int ContID);
-		Territory(const Territory& TerrObj);//Copy constructor for Territory class
+	int ContinentId; // Potentially to know which continent the territory belongs to?
 
-		~Territory();
+public:
+	Territory(int TerrID, std::string TerrName, int ContID);
+	Territory(const Territory &TerrObj); // Copy constructor for Territory class
 
-		Territory& operator=(const Territory& TerrObj);
-		
+	~Territory();
 
-		//methods
-		void addAdjTerr(Territory* x);
+	Territory &operator=(const Territory &TerrObj);
 
+	// methods
+	void addAdjTerr(Territory *x);
 
-		friend ostream& operator<<(ostream& os, const Territory& TerrObj);
-		friend class Map;
-		friend struct Continent;
+	friend ostream &operator<<(ostream &os, const Territory &TerrObj);
+	friend class Map;
+	friend struct Continent;
 };
 
-struct Continent {
+struct Continent
+{
 	int ContinentID;
 	string ContinentName;
 	int Bonus;
-	vector<Territory*> arrOfTerrInContinent;
+	vector<Territory *> arrOfTerrInContinent;
 
-	//Constructors
+	// Constructors
 	Continent(int contID, string contName, int bonus);
-	Continent(const Continent& ContObj);
+	Continent(const Continent &ContObj);
 
-	//Destructor
+	// Destructor
 	~Continent();
 
-	//assignment operator
-	Continent& operator=(const Continent& ContObj);
+	// assignment operator
+	Continent &operator=(const Continent &ContObj);
 
-	//methods
-	void addTerritoryToContinent(Territory* terr);
-	
-	friend ostream& operator<<(ostream& os, const Continent& Contobj);
-	
-	
+	// methods
+	void addTerritoryToContinent(Territory *terr);
 
+	friend ostream &operator<<(ostream &os, const Continent &Contobj);
 };
-
 
 class Map
 {
 private:
-	vector<Continent*> ContinentPointerArray;//Here we're creating the arrays of pointers to Continent and Territory objects
-	vector<Territory*> TerritoryPointerArray;
+	vector<Continent *> ContinentPointerArray; // Here we're creating the arrays of pointers to Continent and Territory objects
+	vector<Territory *> TerritoryPointerArray;
 	int nbOfContinents;
 	int nbOfTerritories;
-	/*Continent* Cont;
-	Territory* Terr;*/
-	
 
 public:
-	//Constructors
-	Map(const Map& MapObj);
+	// Constructors
+	Map(const Map &MapObj);
 	Map();
 
-	//Destructor
+	// Destructor
 	~Map();
-	//Overloaded assignment operator
-	Map& operator=(const Map& MapObj);
+	// Overloaded assignment operator
+	Map &operator=(const Map &MapObj);
 
-	//Methods
-	void addContToContVector(Continent* Cont);
-	void addTerrToTerrVector(Territory* Terr);
+	// Methods
+	void addContToContVector(Continent *Cont);
+	void addTerrToTerrVector(Territory *Terr);
 	int getContId(string ContName);
-	Continent* getContinent(string name);
+	Continent *getContinent(string name);
 	void validate();
 	bool isMapConnected();
 	bool isContinentsconected();
 	bool isBelongOneContinent();
-	void DFS(const Territory* Terr, vector <string>& visited);
-	void continentDFS(const Territory* Terr, vector<string>& visited);
-	Territory* getTerrObjByName(string TerrName);
-
-	
+	void DFS(const Territory *Terr, vector<string> &visited);
+	void continentDFS(const Territory *Terr, vector<string> &visited);
+	Territory *getTerrObjByName(string TerrName);
 
 	friend class MapLoader;
 
-	friend ostream& operator<<(ostream& os, const Map& mapObjPointer);
-
+	friend ostream &operator<<(ostream &os, const Map &mapObjPointer);
 };
 
-class MapLoader {
+class MapLoader
+{
 	int ContinentCounter;
 	int TerritoryCounter;
-	
 
 public:
-	
 	void readFile(string fileName);
 	MapLoader();
-	MapLoader(const MapLoader& MapLObj);
-	//Destructor
+	MapLoader(const MapLoader &MapLObj);
+	// Destructor
 	~MapLoader();
-	MapLoader& operator=(const MapLoader& MapLObj);
-	friend ostream& operator<<(ostream& os, const MapLoader& mapLoaderObj);
+	MapLoader &operator=(const MapLoader &MapLObj);
+	friend ostream &operator<<(ostream &os, const MapLoader &mapLoaderObj);
 	friend class Map;
-	
 };
--- Warzone/Source/Map/MapDriver.cpp
+++ Warzone/Source/Map/MapDriver.cpp
@@ -1,21 +1,20 @@
 #include "Map/MapDriver.h"
+#include <Map/Map.h>
 #include <fstream>
 #include <iostream>
 #include <regex>
 #include <sstream>
 #include <string>
-#include <Map/Map.h>
 
+void testLoadMaps()
+{
+    string fileName = "0";
 
-
-void testLoadMaps() {
-    string fileName="0";
-    
-    while (fileName.compare("null")!=0) {
-        
+    while (fileName.compare("null") != 0)
+    {
         cout << "Please enter the name of the file you'd like to load, if there is no other file you would like to try, enter null : ";
         getline(cin, fileName);
-        MapLoader* mapl1 = new MapLoader;
+        MapLoader *mapl1 = new MapLoader;
         mapl1->readFile(fileName.c_str());
     }
 }
