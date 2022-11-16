c12e3af2a18bbae3f20c1a689ec1eddd7ece3bd4
Runze Zhu <z_runze@live.concordia.ca>
2022-11-16 14:46:49 -0500

    Update GameEngineDriver.cpp

--- Warzone/Source/GameEngine/GameEngineDriver.cpp
+++ Warzone/Source/GameEngine/GameEngineDriver.cpp
@@ -26,3 +26,27 @@ void testStartupPhase()
     }
 }
 
+void testMainGameLoop()
+{
+    cout << "----------------------------------------------------------\n"
+        << "----------------------------------------------------------\n"
+        << "\t** PART 3: MAIN GAME LOOP**\n\n";
+
+    CommandProcessor* commandProcessor = nullptr;
+    GameEngine::State currentState = GameEngine::State::End;
+
+    while (commandProcessor == nullptr || currentState == GameEngine::State::End) {
+        GameEngine* gameEngine = new GameEngine();
+        currentState = GameEngine::State::Start;
+        commandProcessor = gameEngine->initializeCommandProcessor();
+        while (currentState != GameEngine::State::End)
+        {
+            currentState = gameEngine->startupPhase(gameEngine->getState(), commandProcessor);
+        }
+        if (commandProcessor != nullptr)
+        {
+            delete commandProcessor;
+            delete gameEngine;
+        }
+    }
+}
