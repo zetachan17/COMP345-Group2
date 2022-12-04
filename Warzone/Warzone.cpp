c2e6234f531d55c69b01c4b4f66b8ab7d38ec0c8
Matthew Kevork <matthew.kevork@hotmail.com>
2022-12-04 00:31:40 -0500

    update main driver

--- Warzone/Warzone.cpp
+++ Warzone/Warzone.cpp
@@ -1,24 +1,15 @@
 // Warzone.cpp : This file contains the 'main' function. Program execution begins and ends there.
 
-#include "Map/MapDriver.h"
-#include "Player/PlayerDriver.h"
-#include "Orders/OrdersDriver.h"
-#include "Cards/CardsDriver.h"
-#include "GameEngine/GameEngineDriver.h"
-#include "CommandProcessing/CommandProcessingDriver.h"
-#include "LoggingObserver/LoggingObserverDriver.h"
+#include "PlayerStrategies/PlayerStrategiesDriver.h"
 #include <iostream>
 
 int main()
 {
+    //this seed is necessary for rand to behave properly
+    srand(time(0));
+
     //P1
-    testCommandProcessor();
+    testPlayerStrategies();
     //P2
-    //testStartupPhase();
-    //P3
-    //testMainGameLoop();
-    //P4
-    //testOrderExecution();
-    //P5
-    //testLoggingObserver();
+
 }
